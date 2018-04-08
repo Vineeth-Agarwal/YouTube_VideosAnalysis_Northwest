9b0884379197895ad93d613a74016c6a8941e0a5
charan <S530748@nwmissouri.edu>
2018-04-08 16:58:51 -0500

    added comments in code - Charan

--- Video_Sentiment (Charan)/Video_Sentiment_Mapper.py
+++ Video_Sentiment (Charan)/Video_Sentiment_Mapper.py
@@ -1,8 +1,10 @@
-i = open("UScomments.csv", "r")
-o = open("o.txt", "w")
+i = open("UScomments.csv", "r")   # open file, read-only
+o = open("o.txt", "w")   # open file, write
 
 for line in i:
+  #It splits the each line of code by ,
   data = line.strip().split(',')
+  #Checks whether there are 4 parts
   if (len(data) == 4):
     id, text, likes, replies = data
     o.write(id + "\t" + likes+ "\n")
--- Video_Sentiment (Charan)/Video_Sentiment_Sorter.py
+++ Video_Sentiment (Charan)/Video_Sentiment_Sorter.py
@@ -1,6 +1,7 @@
 n = open("o.txt","r")  # open file, read-only
 s = open("s.txt", "w") # open file, write
 lines = n.readlines()
+#Sorts the data in alphabetical order
 lines.sort()
 
 for line in lines:
