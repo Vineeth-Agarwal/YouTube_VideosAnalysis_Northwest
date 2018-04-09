001959c0875aa1a6e2f966be6a76205f3de33dca
Vineeth Agarwal <vineeth.agarwal06@gmail.com>
2018-04-08 22:06:35 -0500

    removed unecessary commented code and added appropriate comments and ran the mapper again with clean code

--- Top_5_Trending_Android_Video_Channels (Vineeth)/Android_Channels_Mapper.py
+++ Top_5_Trending_Android_Video_Channels (Vineeth)/Android_Channels_Mapper.py
@@ -1,6 +1,6 @@
 # Open the input and output files
 input=open("../data/USvideos.csv","r")
-output=open("./Android_Channels_Mapper_OutInt.txt","w")
+output=open("./Android_Channels_Mapper_Out.txt","w")
 count=1
 
 # Reading the input file containing the fields related to each video
@@ -17,23 +17,12 @@ for line in input:
             video_id,title,channel_title,category_id,tags,views,likes,dislikes,comment_total,thumbnail_link,date,=data
 
             # If the tag field had Android or android in it, write it to mapper output file.
-            if tasgs.find("Android")==-1 and tags.find("android")==-1:
+            if tags.find("Android")==-1 and tags.find("android")==-1:
                 continue
             else:
                 # print("{0}\t{1}\t{2}\n".format(views,channel_title,tags))
-                output.write(views)            
+                output.write("{0}\t{1}\t{2}\n".format(views,channel_title,tags))            
 
 # closing files
 input.close()
 output.close()
-
-
-# # sorting output of mapper by views
-# si = open("./Android_Channels_Mapper_Out.txt", "r")
-# so = open("./Android_Channels_Reducer_in.txt", "w")
-# lines = si.readlines()
-# lines.sort()
-# for line in lines:
-# 	so.write(line)
-# si.close()
-# so.close()
