f57190c04d4880f8fb52453fa9b93c48e7257621
Vineeth Agarwal <vineeth.agarwal06@gmail.com>
2018-04-08 22:01:50 -0500

    Implemented auto sort functionality of my interest

--- Top_5_Trending_Android_Video_Channels (Vineeth)/Android_Channels_Mapper.py
+++ Top_5_Trending_Android_Video_Channels (Vineeth)/Android_Channels_Mapper.py
@@ -1,6 +1,6 @@
 # Open the input and output files
 input=open("../data/USvideos.csv","r")
-output=open("./Android_Channels_Mapper_Out.txt","w")
+output=open("./Android_Channels_Mapper_OutInt.txt","w")
 count=1
 
 # Reading the input file containing the fields related to each video
@@ -15,24 +15,25 @@ for line in input:
     # If data is valid in a row, assign it in 11 variables as below
     if(len(data)==11):
             video_id,title,channel_title,category_id,tags,views,likes,dislikes,comment_total,thumbnail_link,date,=data
+
             # If the tag field had Android or android in it, write it to mapper output file.
-            if tags.find("Android")==-1 and tags.find("android")==-1:
+            if tasgs.find("Android")==-1 and tags.find("android")==-1:
                 continue
             else:
-                output.write("{0}\t{1}\t{2}\n".format(views,channel_title,tags))            
+                # print("{0}\t{1}\t{2}\n".format(views,channel_title,tags))
+                output.write(views)            
 
 # closing files
 input.close()
 output.close()
 
 
-
-# sorting output of mapper by views
-si = open("./Android_Channels_Mapper_Out.txt", "r")
-so = open("./Android_Channels_Reducer_in.txt", "w")
-lines = si.readlines()
-lines.sort()
-for line in lines:
-	so.write(line)
-si.close()
-so.close()
+# # sorting output of mapper by views
+# si = open("./Android_Channels_Mapper_Out.txt", "r")
+# so = open("./Android_Channels_Reducer_in.txt", "w")
+# lines = si.readlines()
+# lines.sort()
+# for line in lines:
+# 	so.write(line)
+# si.close()
+# so.close()
