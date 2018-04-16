ae96cae4c58069411e928146e6225b526115a002
Ashok Atkuri <31717364+S530458@users.noreply.github.com>
2018-04-15 19:32:55 -0500

    Top5Trending Videos output mapped to categories

--- Top_5_Trending_Video_Categories (Ashok)/Top5Trending.py
+++ Top_5_Trending_Video_Categories (Ashok)/Top5Trending.py
@@ -1,18 +1,27 @@
 i = open("CategoryCount_TopicWise.txt","r")
 o = open("Top5Trending.txt","w")
 
-trending_dict = {}
+tupples_array = []
 
-lines = i.readlines()
-lines.sort(reverse=True)
+cat_id_mapping = {2:'Autos & Vehicles',1:'Film & Animation',
+                  10:'Music',15:'Pets & Animals',17:'Sports',
+                 19:'Travel & Events',20:'Gaming',22:'People & Blogs',
+                 23:'Comedy',24:'Entertainment',25:'News & Politics',
+                 26:'Howto & Style',27:'Education',28:'Science & Technology',
+                  29:'Nonprofits & Activism',43:'Shows'}
 
-for  line in lines:
-    data = line.strip().split("\t")
-    if data != " " or data != None:
-        cCount,cid = data    
-        trending_dict[cid] = cCount
-        print(trending_dict)
-    #o.write(line)
+for line in i:
+    data = line.strip().split(",")
+    cCount, cCid = data
+    c = int(cCid)
+    for cid in cat_id_mapping.keys():
+        if c == cid:
+            tupple = (int(cCount),cat_id_mapping[c])
+            tupples_array.append(tupple)
+
+sorted_array = (sorted(tupples_array, key=lambda array_elements: array_elements[0], reverse=True))
+print(sorted_array)
+o.write("\n".join('%s, %s' % new_array_elements for new_array_elements in sorted_array))
 
 i.close()
 o.close()    
--- Top_5_Trending_Video_Categories (Ashok)/Trending_Video_Category_Reducer.py
+++ Top_5_Trending_Video_Categories (Ashok)/Trending_Video_Category_Reducer.py
@@ -13,7 +13,7 @@ for line in i:
 		if cID == category_id:
 			cCount += 1
 		else:
-			o.write(str(cCount) + "\t"+ "Category id: "+ cID +"\n")
+			o.write(str(cCount) + ","+ cID)
 			print("Category id: "+ cID + "\t"+ str(cCount) +"\n")
 			cID = category_id			
 			cCount = 1	
